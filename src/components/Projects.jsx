b3a86591d3a1acd519a7fce5d794da9a5ebeaae3
Iván Sebastián Alguacil González <ivanalguacil11@gmail.com>
2026-03-01 03:40:51 +0100

    relative path image problem

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
       <div className="container my-5" id="Project1" style={{backgroundColor: "rgb(9 9 9)", padding: "1rem", borderRadius: "0.5rem", border: "2px solid #222"}}>
         <div className="row align-items-center">
           <div className="col-md-3 text-center mb-4 mb-md-0">
-            <img src="public\Projects\Portada.jpg" 
+            <img src="/Projects/Portada.jpg" 
                 alt="Proyecto" 
                 className="project-img img-fluid"
                 style={{borderRadius: "0.5rem"}}/>
@@ -18,15 +18,14 @@ export default function Projects() {
             <h3 className="text-2xl font-bold mb-2" >Echoes of the Abyss</h3>
             <p>Demo de videojuego "Roguelike" 3D con vista isométrica desarrollado en Unity.</p>
             <div className="d-flex flex-wrap justify-content-center gap-3 mt-3">
-              <img src="public\Technologies\Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
             </div>
             <div className="text-center mt-3">
               <a href="https://github.com/ivanalggon/Echoes-Of-The-Abyss" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Ver en GitHub</a>
             </div>
-
           </div>
         </div>
       </div>
@@ -34,7 +33,7 @@ export default function Projects() {
     <div className="container my-5" id="Project1" style={{backgroundColor: "rgb(9 9 9)", padding: "1rem", borderRadius: "0.5rem", border: "2px solid #222"}}>
         <div className="row align-items-center">
           <div className="col-md-3 text-center mb-4 mb-md-0">
-            <img src="public\Projects\Portada.jpg" 
+            <img src="/Projects/Portada.jpg" 
                 alt="Proyecto" 
                 className="project-img img-fluid"
                 style={{borderRadius: "0.5rem"}}/>
@@ -43,15 +42,14 @@ export default function Projects() {
             <h3 className="text-2xl font-bold mb-2" >Echoes of the Abyss</h3>
             <p>Demo de videojuego "Roguelike" 3D con vista isométrica desarrollado en Unity.</p>
             <div className="d-flex flex-wrap justify-content-center gap-3 mt-3">
-              <img src="public\Technologies\Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
             </div>
             <div className="text-center mt-3">
               <a href="https://github.com/ivanalggon/Echoes-Of-The-Abyss" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Ver en GitHub</a>
             </div>
-
           </div>
         </div>
       </div>
@@ -59,7 +57,7 @@ export default function Projects() {
       <div className="container my-5" id="Project1" style={{backgroundColor: "rgb(9 9 9)", padding: "1rem", borderRadius: "0.5rem", border: "2px solid #222"}}>
         <div className="row align-items-center">
           <div className="col-md-3 text-center mb-4 mb-md-0">
-            <img src="public\Projects\Portada.jpg" 
+            <img src="/Projects/Portada.jpg" 
                 alt="Proyecto" 
                 className="project-img img-fluid"
                 style={{borderRadius: "0.5rem"}}/>
@@ -68,15 +66,14 @@ export default function Projects() {
             <h3 className="text-2xl font-bold mb-2" >Echoes of the Abyss</h3>
             <p>Demo de videojuego "Roguelike" 3D con vista isométrica desarrollado en Unity.</p>
             <div className="d-flex flex-wrap justify-content-center gap-3 mt-3">
-              <img src="public\Technologies\Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
-              <img src="public\Technologies\substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Unity-logo.png" alt="Unity" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/Csharp_Logo.png" alt="C#" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/3ds-max.png" alt="3ds-max" className="img-fluid" style={{width: "50px", height: "50px"}}/>
+              <img src="/Technologies/substance-painter-logo.png" alt="Substance Painter" className="img-fluid" style={{width: "50px", height: "50px"}}/>
             </div>
             <div className="text-center mt-3">
               <a href="https://github.com/ivanalggon/Echoes-Of-The-Abyss" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Ver en GitHub</a>
             </div>
-
           </div>
         </div>
       </div>
