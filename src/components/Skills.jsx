9d5443f3128ce1f98d64bcc2b5c7e506f76c26cf
Iván Sebastián Alguacil González <ivanalguacil11@gmail.com>
2026-08-26 23:27:59 +0200

    updated relative path

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,15 +13,15 @@ export default function Skills() {
           <h1>Front-end</h1>
 
           <div className="skill-gallery">
-            <img src="public\Technologies\Html logo.png" alt="Front-end 1" />
-            <img src="public\Technologies\Css logo.png" alt="Front-end 2" />
-            <img src="public\Technologies\JavaScript logo.png" alt="Front-end 3" />
-            <img src="public\Technologies\Node.js logo.png" alt="Front-end 4" />
-            <img src="public\Technologies\Bootstrap logo.png" alt="Front-end 7" />
-            <img src="public\Technologies\React logo.png" alt="Front-end 6" />
-            <img src="public\Technologies\Angular logo.png" alt="Front-end 8" />
-            <img src="public\Technologies\Git.png" alt="Front-end 8" />
-            <img src="public\Technologies\Github.png" alt="Front-end 8" />
+            <img src="/public/Technologies/Html logo.png" alt="Front-end 1" />
+            <img src="/public/Technologies/Css logo.png" alt="Front-end 2" />
+            <img src="/public/Technologies/JavaScript logo.png" alt="Front-end 3" />
+            <img src="/public/Technologies/Node.js logo.png" alt="Front-end 4" />
+            <img src="/public/Technologies/Bootstrap logo.png" alt="Front-end 7" />
+            <img src="/public/Technologies/React logo.png" alt="Front-end 6" />
+            <img src="/public/Technologies/Angular logo.png" alt="Front-end 8" />
+            <img src="/public/Technologies/Git.png" alt="Front-end 8" />
+            <img src="/public/Technologies/Github.png" alt="Front-end 8" />
           </div>
 
           <p>
@@ -35,11 +35,11 @@ export default function Skills() {
           <h1>Back-end</h1>
           <br />
           <div className="skill-gallery">
-            <img src="public\Technologies\Csharp_Logo.png" alt="Backend 1" />
-            <img src="public\Technologies\MongoDB logo.png" alt="Backend 2" />
-            <img src="public\Technologies\Python logo.png" alt="Backend 3" />
-            <img src="public\Technologies\Git.png" alt="Backend 4" />
-            <img src="public\Technologies\Github.png" alt="Backend 5" />
+            <img src="/public/Technologies/Csharp_Logo.png" alt="Backend 1" />
+            <img src="/public/Technologies/MongoDB logo.png" alt="Backend 2" />
+            <img src="/public/Technologies/Python logo.png" alt="Backend 3" />
+            <img src="/public/Technologies/Git.png" alt="Backend 4" />
+            <img src="/public/Technologies/Github.png" alt="Backend 5" />
           </div>
           <br />
           <p>
@@ -53,13 +53,13 @@ export default function Skills() {
           <h1>Videojuegos</h1>
 
           <div className="skill-gallery">
-            <img src="public\Technologies\3ds-max.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\Blender.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\substance-painter-logo.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\Zbrush Logo.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\Unity-logo.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\Git.png" alt="Videojuegos 1" />
-            <img src="public\Technologies\Github.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/3ds-max.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/Blender.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/substance-painter-logo.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/Zbrush Logo.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/Unity-logo.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/Git.png" alt="Videojuegos 1" />
+            <img src="/public/Technologies/Github.png" alt="Videojuegos 1" />
           </div>
 
           <p>
